ce2d80e90f793091cf54894b8712bdc2d968982b
PuppyDiary <582781785@qq.com>
2021-04-14 22:36:46 +0800

    Create Exercise2.js

---
+++ Exercise2.js
@@ -0,0 +1,11 @@
+function check(){
+    var x = document.getElementById("answer").value;
+    
+      if (x == "1"){
+      alert("Yes,you are right!");
+      }
+    else{
+      alert("Sorry,you are wrong.Think again?");
+      }
+    }
+ 
